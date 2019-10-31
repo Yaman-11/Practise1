fa62972e80873716be07060b7814376d4cef57e8
Yaman <yamanniroula11@gmail.com>
2019-10-31 16:49:36 +0530

    Reverting "printo"
    This reverts commit 88fc5f26293d597ce9f637c123c708e5c2c31357.

--- src/main/java/com/example/practise/Practise/PractiseApplication.java
+++ src/main/java/com/example/practise/Practise/PractiseApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PractiseApplication {
 
     public static void main(String[] args) {
-        System.out.println("NESDFDghfj");
         SpringApplication.run(PractiseApplication.class, args);
     }
 
